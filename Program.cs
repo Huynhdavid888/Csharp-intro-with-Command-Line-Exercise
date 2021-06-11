ba13a4973c8b708624aa0459506a17f7979fdbdb
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-11 14:35:40 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#17 classmates and instructor!");
+        }
+    }
+}
